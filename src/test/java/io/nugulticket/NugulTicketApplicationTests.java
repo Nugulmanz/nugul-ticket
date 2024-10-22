4f1c3859340b3e2e3a6b5cffd3d139f4952fd4ad
vieve <171054334+jay1864@users.noreply.github.com>
2024-10-22 13:12:47 +0900

    init

---
+++ src/test/java/io/nugulticket/NugulTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nugulticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NugulTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
