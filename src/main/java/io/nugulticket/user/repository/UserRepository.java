d5b48f8d155d0d444abb137ebacf0f15b9203a23
vieve <171054334+jay1864@users.noreply.github.com>
2024-10-24 15:31:16 +0900

    update: UserRepository default 메소드 추가

--- src/main/java/io/nugulticket/user/repository/UserRepository.java
+++ src/main/java/io/nugulticket/user/repository/UserRepository.java
@@ -11,4 +11,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
     Optional<User> findBySocialId(Long socialId);
+
+    default User findUserById(Long id) {
+        return findById(id).orElseThrow(
+                () -> new RuntimeException("사용자를 찾을 수 없습니다.")
+        );
+    }
+
+    default User findUserByEmail(String email) {
+        return findByEmail(email).orElseThrow(
+                () -> new RuntimeException("해당 이메일의 사용자를 찾을 수 없습니다.")
+        );
+    }
 }
