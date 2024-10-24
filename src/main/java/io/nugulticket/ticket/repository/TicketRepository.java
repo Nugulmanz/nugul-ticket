452d7816c4efd4574a6e7bc56a3e520f83fe8557
KTJ-1650 <xowns1650@gmail.com>
2024-10-25 08:17:19 +0900

    fix: buyerId를 userId로 수정

--- src/main/java/io/nugulticket/ticket/repository/TicketRepository.java
+++ src/main/java/io/nugulticket/ticket/repository/TicketRepository.java
@@ -15,7 +15,6 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Optional<Ticket> findByUser_IdAndTicketId(Long userId, Long ticketId);
 
     Optional<Ticket> findByUser_IdAndTicketIdAndEvent_EventId(Long userId, Long ticketId, Long eventId);
-    List<Ticket> findAllByStatusAndBuyerId(TicketStatus status, Long buyerId);
     /**
      * 해당 유저( buyerId )가 구매한 Ticket중에 status가 동일한 상태인 Ticket에 대해
      * Event, Seat를 fetch join하여 반환하는 쿼리
