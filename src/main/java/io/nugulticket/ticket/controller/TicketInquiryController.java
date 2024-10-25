1768e820c50c82ac5e2705d9e70129c62393c3c6
Juho <ljlh9509@gmail.com>
2024-10-25 15:39:10 +0900

    fix: url 수정

--- src/main/java/io/nugulticket/ticket/controller/TicketInquiryController.java
+++ src/main/java/io/nugulticket/ticket/controller/TicketInquiryController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tickets")
+@RequestMapping("/api/tickets")
 @RequiredArgsConstructor
 public class TicketInquiryController {
     private final TicketInquiryService ticketInquiryService;
