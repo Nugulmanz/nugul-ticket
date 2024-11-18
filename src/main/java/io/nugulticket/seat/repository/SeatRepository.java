5e088a14bcd7c5884f1f9023c12c18c402487880
vieve <171054334+jay1864@users.noreply.github.com>
2024-11-19 00:08:24 +0900

    feat: Get api/seats/v1/eventTimeId API 구현
    특정 공연의 특정 회차에서 모든 좌석 정보를 반환하는 기능

--- src/main/java/io/nugulticket/seat/repository/SeatRepository.java
+++ src/main/java/io/nugulticket/seat/repository/SeatRepository.java
@@ -3,5 +3,8 @@ package io.nugulticket.seat.repository;
 import io.nugulticket.seat.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatRepository extends JpaRepository<Seat,Long> {
+    List<Seat> findByEventTimeId(int eventTime_id);
 }
